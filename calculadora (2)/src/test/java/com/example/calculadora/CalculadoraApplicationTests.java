230236a9cbc052ee27a90c5d60c8f538f7b1fa34
Melisa Hinojosa <melibhv@gmail.com>
2022-11-17 21:28:09 -0300

    testapi

--- calculadora (2)/src/test/java/com/example/calculadora/CalculadoraApplicationTests.java
+++ calculadora (2)/src/test/java/com/example/calculadora/CalculadoraApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.calculadora;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.calculadora.Controller.CalculadoraController;
 
 @SpringBootTest
 class CalculadoraApplicationTests {
 
 	@Test
 	void suma() {
+        assertEquals(CalculadoraController.suma(1,2), 3);
 	}
+
+    void suma2() {
+        assertNotEquals(CalculadoraController.suma(1,2), 5);
+	}
+    void resta() {
+        assertEquals(CalculadoraController.resta(1,1), 0);
+	}
+
+    void resta2() {
+        assertNotEquals(CalculadoraController.resta(1,2), 4);
+	}
+    void multiplicar() {
+        assertEquals(CalculadoraController.multiplicar(2,2), 4);
+	}
+
+    void multiplicar2() {
+        assertNotEquals(CalculadoraController.multiplicar(5,2), 11);
+	}
+    void dividir() {
+        assertEquals(CalculadoraController.dividir(10,2), 5);
+	}
+
+    void dividir() {
+        assertNotEquals(CalculadoraController.dividir(1,2), 0);
+	}
+
 
 }
